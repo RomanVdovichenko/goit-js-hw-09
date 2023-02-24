8e42d2b2e8b29cadcd3a72692b208d64f3741114
Roman Vdovichenko <vrn6870@gmail.com>
2023-02-24 16:55:22 +0200

    new5

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -50,8 +50,8 @@ function timerStart() {
                 timer -= 1000;
                 updateTimer();
             } else {
-                clearInterval(id);
                 Notiflix.Notify.success("Time is up!");
+                clearInterval(id);
             }                
     },1000)
 }
