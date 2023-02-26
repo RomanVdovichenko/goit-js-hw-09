97708ef7253df24b954e71c50f139fcd63eebea7
RomanVdovichenko <104017963+RomanVdovichenko@users.noreply.github.com>
2023-02-26 05:12:05 +0000

    Deploying to gh-pages from @ RomanVdovichenko/goit-js-hw-09@2719ce928b424736e81f755fa5f0dff74011dd48 🚀

---
+++ 01-color-switcher.a8bc396d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let d=null;e.setAttribute("disabled",""),t.addEventListener("click",(function(){t.setAttribute("disabled",""),e.removeAttribute("disabled"),d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),e.setAttribute("disabled",""),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.a8bc396d.js.map
